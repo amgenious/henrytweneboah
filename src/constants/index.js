3f5087b19908905c8ad3785b0fe2edf441a3763f
amgenious <henrytweneboah956@gmail.com>
2023-07-20 00:19:23 +0000

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,256 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    ecg,
+    bismuth,
+    threejs,
+    techathon,
+    nubsuenr,
+    urc,
+    urc_learning_hub,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Frontend Developer",
+      icon: web,
+    },
+    {
+      title: "Mobile App Developer (Flutter)",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Graphic Designer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
+  ];
+  
+  const experiences = [
+    {
+      title: "Frontend Developer (Intern)",
+      company_name: "Bismuth",
+      icon: bismuth,
+      iconBg: "#ffff",
+      date: "September 2022 - January 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implemented responsive design and ensured cross-browser compatibility using chrome dev tools.",
+        "Participated in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Frontend Developer (Remote)",
+      company_name: "Techathon",
+      icon: techathon,
+      iconBg: "#E6DEDD",
+      date: "October 2022 - December 2022",
+      points: [
+        "Developedand maintained web applications using Next.ts, Chakra UI and other related technologies.",
+        "Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implemented responsive design and ensured cross-browser compatibility using chrome dev tools.",
+        "Participated in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "IT Support Engineer (Intern)",
+      company_name: "Electricity Company of Ghana (ECG)",
+      icon: ecg,
+      iconBg: "#E6DEDD",
+      date: "October 2021 - November 2021",
+      points: [
+        "Reduced rerouting of network hubs to computer, optimized software applications and operating systems to increase computer throughput for increased productivity",
+        "Troubleshooted networking devices to ensure its optimum efficient use.",
+        "Installed and Updated their system security softwares for their offices and warehouses.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Henry proved me wrong.",
+      name: "Dennis Adonai",
+      designation: "CEO",
+      company: "Bismuth",
+      // image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Henry does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "Techathon",
+      // image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Henry optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Bawa Daniel",
+      designation: "President",
+      company: "URC",
+      // image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Nubs Uenr",
+      web_link:"https://nubs-uenr.com",
+      description:
+        "Web application that enables users to know more about nubs uenr, it vision and missions. Users can get access to books on their e-library platform. Users can get access to previous biblestudies materials and sermons preached on Sundays",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "nodejs",
+          color: "orange-text-gradient",
+        },
+      ],
+      image: nubsuenr,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Uenr Robotics Club (urc)",
+      web_link:"https://urc.onrender.com",
+      description:
+        "Web application that allows users to know more about urc; its members, executives and patron. Users can get access to completed projects on the projects page",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "nodejs",
+          color: "orange-text-gradient",
+        },
+      ],
+      image: urc,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "URC Learning Hub",
+      web_link:"https://urc-codes.github.io/learning_hub_frontend/",
+      description:
+        "A web application where users can get access to free books and videos on some skills such as graphic designing, web development, mobile development, ai and machine learning and general programming",
+      tags: [
+        {
+          name: "reactjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "threejs",
+          color: "pink-text-gradient",
+        },
+        {
+          name:"nodejs",
+          color:"orange-text-gradient"
+        }
+      ],
+      image: urc_learning_hub,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
